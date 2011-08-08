ce83d1cc827dc321c3595137ddd73175f11edb6f
renzo.kottmann@googlemail.com <renzo.kottmann@googlemail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-08-08 15:45:36 +0000

    minor fixes

--- branches/tycho-build/build-all-run.sh
+++ branches/tycho-build/build-all-run.sh
@@ -1,11 +1,17 @@
+#!/bin/bash
 export MAVEN_OPTS="-Xmx1024m"
+
 cd choncms-parent
 mvn clean package
+
 cd ..
 cd bundles
 mvn clean package
+
 cd ..
 cd chon-cms-web-container
+
 mvn clean package
 mvn jetty:run
 
+cd ..
