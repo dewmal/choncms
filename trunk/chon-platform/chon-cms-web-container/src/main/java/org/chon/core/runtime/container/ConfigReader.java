798acbd590c3751f82a0720affc2207674aced68
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-08-09 14:42:57 +0000

    adding support for dropins dir

--- trunk/chon-platform/chon-cms-web-container/src/main/java/org/chon/core/runtime/container/ConfigReader.java
+++ trunk/chon-platform/chon-cms-web-container/src/main/java/org/chon/core/runtime/container/ConfigReader.java
@@ -10,54 +10,48 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class ConfigReader {
 
-	private static final String SYS_PROPS_DIR = System.getProperty("system-properties-dir");
-	private static final String SYS_PROPS_FILE_NAME = System.getProperty("system-properties", "system.properties");
-	
-	public static void readSystemProperties(ServletContext servletContext) throws ConfigurationException {		 
-	//	try {
+	private static final String SYS_PROPS_DIR = System
+			.getProperty("system-properties-dir");
+	private static final String SYS_PROPS_FILE_NAME = System.getProperty(
+			"system-properties", "system.properties");
+
+	public static void readSystemProperties(ServletContext servletContext)
+			throws ConfigurationException {
+
+
+		File sysPropsDir = null;
+		if (SYS_PROPS_DIR != null) {
+			sysPropsDir = new File(SYS_PROPS_DIR);
+		} else {
+			sysPropsDir = new File(servletContext.getRealPath("/WEB-INF/"));
+		}
+
+		File propertiesFile = new File(sysPropsDir, SYS_PROPS_FILE_NAME);
+
+		if (propertiesFile.exists()) {
+			servletContext.log(":::> Reading " + SYS_PROPS_FILE_NAME);
+			PropertiesConfiguration properties = new PropertiesConfiguration(propertiesFile);
 			
-			File sysPropsDir = null;
-			if(SYS_PROPS_DIR != null) {
-				sysPropsDir = new File(SYS_PROPS_DIR);
-			} else {
-				sysPropsDir = new File(servletContext.getRealPath("/WEB-INF/"));
-			}
+			// TODO: put other useful properties
+			properties.setProperty("user.home", System.getProperty("user.home"));
 			
-			File propertiesFile = new File(sysPropsDir, SYS_PROPS_FILE_NAME);
+			initDefaultSystemProperties(properties);
 			
-			
-			
-			if (propertiesFile.exists()) {
-				servletContext.log(":::> Reading " + SYS_PROPS_FILE_NAME);
-				PropertiesConfiguration properties = new PropertiesConfiguration(propertiesFile);
-				//TODO: put other useful properties 
-				properties.setProperty("user.home", System.getProperty("user.home"));
-				
-				//Properties appProperties = new Properties();
-				//appProperties.load(new FileInputStream(propertiesFile));
-				Iterator<?> it = properties.getKeys();
-				
-				//Enumeration<Object> itKeys = appProperties.keys();
-				StringBuffer logBuf = new StringBuffer();
-				while (it.hasNext()) {
-					String k = (String) it.next();
-					String value = properties.getString(k);
-					/*
-					if (value.startsWith("./")) {
-						value = sysPropsDir.getAbsolutePath() + "/"
-								+ value.substring(2);
-						value = new File(value).getCanonicalPath().replaceAll("\\\\", "/");
-					}
-					*/
-					logBuf.append(k + "=" + value + ";");
-					System.getProperties().put(k, value);
-				}
-				servletContext.log(":::> SYSTEM PROPERTIES ["+logBuf.toString()+"]");
-			}
-		//} catch (FileNotFoundException e) {
-		//	servletContext.log("WARN: "+SYS_PROPS_FILE_NAME+" not found", e);
-		//} catch (IOException e) {
-		//	servletContext.log("ERROR: Can not read "+SYS_PROPS_FILE_NAME+" file", e);
-		//}
+			copyToSystemProperties(properties);
+		}
 	}
+	
+	private static void initDefaultSystemProperties(PropertiesConfiguration properties) {
+		System.setProperty("felix.fileinstall.dir", properties.getString("chon.dropins.dir"));
+	}
+	
+	private static void copyToSystemProperties(PropertiesConfiguration properties) {
+		Iterator<?> it = properties.getKeys();
+		while (it.hasNext()) {
+			String k = (String) it.next();
+			String value = properties.getString(k);
+			System.getProperties().put(k, value);
+		}
+	}
+
 }
