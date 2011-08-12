27750f6267eb69d7cdd26922402ac1a3e54cb29f
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-08-12 13:01:08 +0000

    1.0 tags bu misstake, first release at 1.1

--- tags/choncms.starter-1.0/chon-platform/bundles/org.chon.cms.core/src/org/chon/cms/core/auth/AuthenticationProvider.java
+++
@@ -1,6 +0,0 @@
-package org.chon.cms.core.auth;
-
-public interface AuthenticationProvider {
-	public User login(String username, String password) throws InvalidUserException;
-	public boolean logout(User user);
-}
--- tags/choncms.starter-1.0/chon-platform/bundles/org.chon.web/src/org/chon/web/mpac/Module.java
+++
@@ -1,7 +0,0 @@
-package org.chon.web.mpac;
-public interface Module extends ActionHandler {
-	public String getName();
-	public int getOrder();
-	public String getLayout();
-	
-}
