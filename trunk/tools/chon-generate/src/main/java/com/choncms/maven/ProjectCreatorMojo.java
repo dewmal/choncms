8d121b091729a2494eb84a6164ee123109b25445
JVeljan@gmail.com <JVeljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-08-20 23:31:16 +0000

    updating project creation structure

--- trunk/tools/chon-generate/src/main/java/com/choncms/maven/ProjectCreatorMojo.java
+++ trunk/tools/chon-generate/src/main/java/com/choncms/maven/ProjectCreatorMojo.java
@@ -1,28 +1,10 @@
 package com.choncms.maven;
 
-/*
- * Copyright 2001-2005 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-import java.io.File;
 import java.io.IOException;
-
-import org.apache.maven.plugin.MojoExecutionException;
+import java.util.Map;
 
 /**
- * Goal which touches a timestamp file.
+ * Goal which creates new chon based project
  * 
  * @requiresProject false
  * @goal new-project
@@ -31,26 +13,24 @@ public class ProjectCreatorMojo extends AbstractCreatorMojo {
 	
 	private String projectName = "chon.based.project";
 	
-	public void execute() throws MojoExecutionException {		
+	
+	@Override
+	protected Map<String, Object> getTemplateVariables() {
+		Map<String, Object> tplVars = super.getTemplateVariables();
 		try {
 			projectName = getValue("Project Name", projectName);
-			populateTemplateVariables();
-			File base = new File(basedir);
-			base.mkdirs();
-			
-			Resource project = readProjectStructure();
-			
-			project.create(base);
+			tplVars.put("project-name", projectName);
+			tplVars.put("project-groupId", projectName);
+			tplVars.put("project-package", projectName);
+			tplVars.put("project-parent-groupId", projectName);
 		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating project", e);
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+		return tplVars;
 	}
 
-	private void populateTemplateVariables() {
-		templateVarsMap.put("project-name", projectName);
-	}
 
-	@Override
 	protected String getProjectType() {
 		return "project";
 	}
