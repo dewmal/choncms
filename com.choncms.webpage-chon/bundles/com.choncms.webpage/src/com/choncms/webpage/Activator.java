10228a856249d79fce27980c3deecdb2da987608
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-08-24 10:37:09 +0000

    Adding choncms web page "com.choncms.webpage-chon" to svn

---
+++ com.choncms.webpage-chon/bundles/com.choncms.webpage/src/com/choncms/webpage/Activator.java
@@ -0,0 +1,18 @@
+package com.choncms.webpage;
+
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+	@Override
+	protected void registerExtensions(JCRApplication app) {
+		// TODO register extension
+	}
+
+	@Override
+	protected String getName() {
+		return "com.choncms.webpage";
+	}
+	
+}
