1b15818d2e0aff54d53147f546786b562da86e5c
JVeljan@gmail.com <JVeljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2011-09-09 05:35:49 +0000

    adding utils for creation types in code

--- chon-platform/bundles/org.chon.cms.content/src/org/chon/cms/content/Activator.java
+++ chon-platform/bundles/org.chon.cms.content/src/org/chon/cms/content/Activator.java
@@ -4,8 +4,10 @@ import org.chon.cms.content.ext.content.ContentExtension;
 import org.chon.cms.content.ext.label.LabelsExtension;
 import org.chon.cms.content.ext.news.NewsExtension;
 import org.chon.cms.content.ext.utils.UtilsExtension;
+import org.chon.cms.content.impl.SimpleContentNodeFactory;
 import org.chon.cms.core.JCRApplication;
 import org.chon.cms.core.ResTplConfiguredActivator;
+import org.chon.cms.model.content.IContentNodeFactory;
 
 public class Activator extends ResTplConfiguredActivator {
 
@@ -21,6 +23,11 @@ public class Activator extends ResTplConfiguredActivator {
 		app.regExtension("label", new LabelsExtension(app));
 		//app.regExtension("menu", new MenuExtension(app));
 		app.regExtension("news", new NewsExtension(app));
+		//helper for registering node types
+		IContentNodeFactory factory = new SimpleContentNodeFactory();
+		getBundleContext().registerService(SimpleContentNodeFactory.class.getName(), factory, null);
+		getBundleContext().registerService(IContentNodeFactory.class.getName(), factory, null);
+		
 	}
 
 
