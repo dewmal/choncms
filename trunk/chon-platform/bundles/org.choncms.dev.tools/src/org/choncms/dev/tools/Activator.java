4c793798b3ee1a1354eeb1b2c7e20078066510c3
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2012-02-13 16:24:52 +0000

    adding dev tools bundle

---
+++ trunk/chon-platform/bundles/org.choncms.dev.tools/src/org/choncms/dev/tools/Activator.java
@@ -0,0 +1,18 @@
+package org.choncms.dev.tools;
+
+import org.chon.cms.core.JCRApplication;
+import org.chon.cms.core.ResTplConfiguredActivator;
+
+public class Activator extends ResTplConfiguredActivator {
+
+	@Override
+	protected void registerExtensions(JCRApplication app) {
+		app.regExtension(getName(), new DevToolsExtenstion(app, getName()));
+	}
+
+	@Override
+	protected String getName() {
+		return "org.choncms.dev.tools";
+	}
+	
+}
