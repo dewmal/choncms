465145aa53757be2a147e7b33cab28a960b0b3c1
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2012-05-30 16:09:56 +0000

    adding logger to rest libs

--- trunk/chon-platform/chon-rest-feature-bundles/chon-rest-libs/src/main/java/org/choncms/rest/libs/JerseyApplication.java
+++ trunk/chon-platform/chon-rest-feature-bundles/chon-rest-libs/src/main/java/org/choncms/rest/libs/JerseyApplication.java
@@ -7,9 +7,14 @@ import java.util.Set;
 
 import javax.ws.rs.core.Application;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleContext;
 
 public class JerseyApplication extends Application {
+	
+	private static final Log log = LogFactory.getLog(Activator.class);
+	
 	private List<RestServiceProvider> serviceProviders = null;
 	private Set<Object> singletons = null;
 	
@@ -36,8 +41,9 @@ public class JerseyApplication extends Application {
 		return singletons;
 	}
 
-	public void regSingleton(Object svc) {
-		singletons.add(svc);
+	public void regSingleton(Object service) {
+		log.debug("Registering service singleton: " + service.getClass().getName());
+		singletons.add(service);
 	}
 
 	public void reload() {
@@ -50,14 +56,21 @@ public class JerseyApplication extends Application {
 	}
 
 	public void unregSingleton(Object service) {
+		log.debug("Unregistering service singleton: " + service.getClass().getName());
 		singletons.remove(service);
 	}
 
 	public void regServiceProvider(RestServiceProvider sp) {
+		log.debug("Registering service provider: " + sp.getClass().getName());
 		serviceProviders.add(sp);
 	}
 
 	public void unregServiceProvider(RestServiceProvider sp) {
+		log.debug("Unregistering service provider: " + sp.getClass().getName());
 		serviceProviders.remove(sp);
+	}
+
+	public boolean containsServices() {
+		return serviceProviders.size()>0 || singletons.size()>0;
 	}
 }
