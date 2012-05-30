d89c24e2a923edba13ce8ca3767c8c48a21e497d
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2012-05-30 16:09:56 +0000

    adding logger to rest libs

--- chon-platform/chon-rest-feature-bundles/chon-rest-libs/src/main/java/org/choncms/rest/libs/Activator.java
+++ chon-platform/chon-rest-feature-bundles/chon-rest-libs/src/main/java/org/choncms/rest/libs/Activator.java
@@ -6,6 +6,8 @@ import javax.servlet.Servlet;
 import javax.ws.rs.Path;
 import javax.ws.rs.ext.RuntimeDelegate;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -14,6 +16,9 @@ import org.osgi.framework.ServiceRegistration;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 
 public class Activator implements BundleActivator {
+	
+	private static final Log log = LogFactory.getLog(Activator.class);
+	
 	//	String serveltRoot = System.getProperty("chon.rest.root", ALIAS);
 	
 	private static final String ALIAS = "/services";
@@ -23,13 +28,14 @@ public class Activator implements BundleActivator {
 	
 	private JerseyAnnotatedServiceTracker serviceTracker = null;
 	private BundleContext bundleContext;
+	private JerseyApplication app;
 	
 	@Override
 	public void start(BundleContext context) throws Exception {
 		this.bundleContext = context;
 		
 		RuntimeDelegate.setInstance(new com.sun.jersey.server.impl.provider.RuntimeDelegateImpl());
-		JerseyApplication app = new JerseyApplication(context, this);
+		app = new JerseyApplication(context, this);
 		servletContainer = new ServletContainer(app);
 		
 		
@@ -65,19 +71,25 @@ public class Activator implements BundleActivator {
 	}
 
 	public synchronized void reload() throws Exception {
-		if(servletContainer == null) {
-			throw new Exception("Unitialised servletContainer");
-		}
 		
 		if(servletContainerRegRef != null) {
 			servletContainerRegRef.unregister();
 			servletContainerRegRef = null;
 		}
 		
+		if(!app.containsServices()) {
+			return;
+		}
+		
+		if(servletContainer == null) {
+			throw new Exception("Unitialised servletContainer");
+		}
+		
+		
 		String serveltRoot = System.getProperty("chon.rest.root", ALIAS);
 		Hashtable<String, String> props = new Hashtable<String, String>();
 		props.put("alias", serveltRoot);
-		System.out.println("Reloading jersey servler chon.rest.root=" + serveltRoot);
+		log.info("Reloading jersey servlet chon.rest.root=" + serveltRoot);
 		servletContainerRegRef = bundleContext.registerService(Servlet.class.getName(), servletContainer, props);
 	}
 }
