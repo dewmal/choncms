960d4cf4909871e55520400211b493a1e2b16961
jveljan@gmail.com <jveljan@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2012-06-06 11:53:16 +0000

    [maven-release-plugin]  copy for tag choncms.starter-1.3

--- chon-platform/bundles/org.chon.cms.light.mvc/src/org/chon/cms/light/mvc/LightMVCService.java
+++
@@ -1,8 +0,0 @@
-package org.chon.cms.light.mvc;
-
-import java.util.Map;
-
-
-public interface LightMVCService {
-	public void setupController(String root_path, Map<String, Class<? extends AbstractAction>> actionzz) throws Exception;
-}
