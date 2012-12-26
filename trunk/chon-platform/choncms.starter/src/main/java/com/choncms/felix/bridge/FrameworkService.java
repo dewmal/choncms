54f9ab42b6f3e329f61de59266b8627857020e36
renzo.kottmann@gmail.com <renzo.kottmann@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2012-12-26 18:06:59 +0000

    a bit more checking for execution conditions

--- trunk/chon-platform/choncms.starter/src/main/java/com/choncms/felix/bridge/FrameworkService.java
+++ trunk/chon-platform/choncms.starter/src/main/java/com/choncms/felix/bridge/FrameworkService.java
@@ -16,103 +16,113 @@ import org.osgi.framework.Constants;
 import com.choncms.config.ConfigReader;
 import com.choncms.config.ResourceLoader;
 
+/*
+ * Part of the Felix ServletBridge Strategy: https://felix.apache.org/site/apache-felix-http-service.html#ApacheFelixHTTPService-UsingtheServletBridge
+ * 
+ * Code taken from https://svn.apache.org/repos/asf/felix/trunk/http/samples/bridge/src/main/java/org/apache/felix/http/samples/bridge/FrameworkService.java
+ */
 
-public final class FrameworkService
-{
-    private final ServletContext context;
-    private Felix felix;
+public final class FrameworkService {
+	private final ServletContext context;
+	private Felix felix;
 
-    public FrameworkService(ServletContext context)
-    {
-        this.context = context;
-    }
-
-    public void start()
-    {
-        try {
-            doStart();
-        } catch (Exception e) {
-            log("Failed to start framework", e);
-        }
-    }
-
-    public void stop()
-    {
-        try {
-            doStop();
-        } catch (Exception e) {
-            log("Error stopping framework", e);
-        }
-    }
-
-    private void doStart()
-        throws Exception
-    {
-        Felix tmp = new Felix(createConfig());
-        printInfo();
-        tmp.start();
-        this.felix = tmp;
-        log("OSGi framework started", null);
-    }
-
-    private void printInfo() {
-		System.out.println("--------------------------------------------------------------------------------");
-		System.out.println("---------------   Starting Chon Application   ----------------------------------");
-		System.out.println(" - Using work-dir ........ " + System.getProperty("app.work.dir"));
-		System.out.println(" - Plugins dir ........... " + System.getProperty("chon.plugins.dir"));
-		System.out.println(" - Reposotiry dir ........ " + System.getProperty("repo.dir"));
-		System.out.println(" - Site URL .............. " + System.getProperty("siteUrl"));
-		System.out.println("--------------------------------------------------------------------------------");
+	public FrameworkService(ServletContext context) {
+		this.context = context;
 	}
 
-	private void doStop()
-        throws Exception
-    {
-        if (this.felix != null) {
-            this.felix.stop();
-        }
+	public void start() {
+		try {
+			doStart();
+		} catch (Exception e) {
+			log("Failed to start framework", e);
+		}
+	}
 
-        log("OSGi framework stopped", null);
-    }
+	public void stop() {
+		try {
+			doStop();
+		} catch (Exception e) {
+			log("Error stopping framework", e);
+		}
+	}
 
-    private Map<String, Object> createConfig()
-        throws Exception
-    {
-        Properties props = new Properties();
-        InputStream is = ResourceLoader.loadResource("framework.properties", null, this.context);
-        props.load(is);
-
-        HashMap<String, Object> map = new HashMap<String, Object>();
-        for (Object key : props.keySet()) {
-            map.put(key.toString(), props.get(key));
-        }
-        
-        map.put(FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP, Arrays.asList(new ProvisionActivator(this.context)));
-        
-        /**
-         * TODO: check intergrity for configurations
-         * make sure we have valid plugins dir, repository dir and resources...
-         * On initial application run (if application plugins are packaged inside war) 
-         * make sure we can create chon-work-dir and unpack plugins.
-         * Repository and config should also be autocreated 
-         */
-        // read system.properties file
-        ConfigReader.readSystemProperties(this.context);
-        
-        // set felix-cache to temp dir if felx-cache dir not found
-        String felixCacheDirPath = System.getProperty("felix-cache", System.getProperty("java.io.tmpdir") + "/felix-cache");
-        File felixCacheDir = new File(felixCacheDirPath);
-        felixCacheDir.mkdirs();
-        felixCacheDirPath = felixCacheDir.getAbsolutePath();
-        
-        log("Using felix-cache dir: " + felixCacheDirPath, null);
-		map.put(Constants.FRAMEWORK_STORAGE, felixCacheDirPath);
+	private void doStart() throws Exception {
+		this.felix = new Felix(createConfig());
+		felix.start();
+		printInfo();
+		log("OSGi framework started", null);
+	}
+ 
+	private void printInfo() {
+		System.out
+				.println("--------------------------------------------------------------------------------");
+		System.out
+				.println("---------------   Starting Chon CMS Application   ----------------------------------");
+		System.out.println(" - Using work-dir ........ "
+				+ System.getProperty("app.work.dir"));
+		System.out.println(" - Plugins dir ........... "
+				+ System.getProperty("chon.plugins.dir"));
+		System.out.println(" - Reposotiry dir ........ "
+				+ System.getProperty("repo.dir"));
+		System.out.println(" - Site URL .............. "
+				+ System.getProperty("siteUrl")); 
+		System.out
+				.println("--------------------------------------------------------------------------------");
+		System.out.println("BundleContext=" + felix.getBundleContext());
 		
-        return map;
-    }
+	}
 
-    private void log(String message, Throwable cause)
-    {
-        this.context.log(message, cause);
-    }
+	private void doStop() throws Exception {
+		if (this.felix != null) {
+			this.felix.stop();
+		}
+
+		log("OSGi framework stopped", null);
+	}
+
+	private Map<String, Object> createConfig() throws Exception {
+		Properties props = new Properties();
+		// TODO: we actually do not need an own resource loader here: because we
+		// place it anyway in WEB_INF dir
+		InputStream is = ResourceLoader.loadResource("framework.properties",
+				null, this.context);
+		props.load(is);
+
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		for (Object key : props.keySet()) {
+			map.put(key.toString(), props.get(key));
+		}
+
+		map.put(FelixConstants.SYSTEMBUNDLE_ACTIVATORS_PROP,
+				Arrays.asList(new ProvisionActivator(this.context)));
+
+		/**
+		 * TODO: check integrity of configuration make sure we have valid
+		 * plugins dir, repository dir and resources... On initial application
+		 * run (if application plugins are packaged inside war) make sure we can
+		 * create chon-work-dir and unpack plugins. Repository and config should
+		 * also be auto-created
+		 */
+		// read system.properties file
+		ConfigReader.readSystemProperties(this.context);
+
+		// set felix-cache to temp dir if felx-cache dir not found
+		String felixCacheDirPath = System.getProperty("felix-cache",
+				System.getProperty("java.io.tmpdir") + "/felix-cache");
+		File felixCacheDir = new File(felixCacheDirPath);
+		// we do not need to have non-proofed code for creating it, Felix does
+		// it, see
+		// https://felix.apache.org/site/apache-felix-framework-bundle-cache.html
+		// felixCacheDir.mkdirs();
+		felixCacheDirPath = felixCacheDir.getAbsolutePath();
+
+		log("Using felix-cache dir: " + felixCacheDirPath, null);
+		map.put(Constants.FRAMEWORK_STORAGE, felixCacheDirPath);
+
+		return map;
+	}
+
+	private void log(String message, Throwable cause) {
+		this.context.log(message, cause);
+	}
 }
--- trunk/chon-platform/choncms.starter/src/main/java/com/choncms/felix/bridge/StartupListener.java
+++ trunk/chon-platform/choncms.starter/src/main/java/com/choncms/felix/bridge/StartupListener.java
@@ -3,19 +3,22 @@ package com.choncms.felix.bridge;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
-public final class StartupListener
-    implements ServletContextListener
-{
-    private FrameworkService service;
+/*
+ * Part of the Felix ServletBridge Strategy: https://felix.apache.org/site/apache-felix-http-service.html#ApacheFelixHTTPService-UsingtheServletBridge
+ * 
+ * Code taken from https://svn.apache.org/repos/asf/felix/trunk/http/samples/bridge/src/main/java/org/apache/felix/http/samples/bridge/StartupListener.java
+ */
 
-    public void contextInitialized(ServletContextEvent event)
-    {
-        this.service = new FrameworkService(event.getServletContext());
-        this.service.start();
-    }
+public final class StartupListener implements ServletContextListener {
+	private FrameworkService service;
 
-    public void contextDestroyed(ServletContextEvent event)
-    {
-        this.service.stop();
-    }
+	public void contextInitialized(ServletContextEvent event) {
+		this.service = new FrameworkService(event.getServletContext());
+		this.service.start();
+		
+	}
+
+	public void contextDestroyed(ServletContextEvent event) {
+		this.service.stop();
+	}
 }
