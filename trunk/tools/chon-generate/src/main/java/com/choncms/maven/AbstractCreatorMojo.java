9b9a4863832597cdd3a8e9b3b16a44a3099ce065
renzo.kottmann@gmail.com <renzo.kottmann@gmail.com@bbf6abbb-87fe-4cb4-6891-5151353d81b6>
2013-09-24 16:28:49 +0000

    fixed to include new http://resources.megx.net online archive

--- trunk/tools/chon-generate/src/main/java/com/choncms/maven/AbstractCreatorMojo.java
+++ trunk/tools/chon-generate/src/main/java/com/choncms/maven/AbstractCreatorMojo.java
@@ -13,62 +13,61 @@ import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.project.MavenProject;
 
 public abstract class AbstractCreatorMojo extends AbstractMojo {
-	protected static final String RES_BASE_URL = "http://resources.choncms.com/";
-	protected String resourceLocation = RES_BASE_URL + "default";
-	
-	 /**
+    protected static final String RES_BASE_URL = "http://resources.megx.net/";
+    protected String resourceLocation = RES_BASE_URL + "default";
+
+    /**
      * @parameter default-value="${project}"
      * @required
      * @readonly
      */
     protected MavenProject project;
-    
-	protected abstract String getProjectType();
 
-	protected Map<String, Object> getTemplateVariables() {
-		Map<String, Object> templateVarsMap = new HashMap<String, Object>();
-		templateVarsMap.put("project-groupId", "com.choncms");
-		templateVarsMap.put("project-package", "com.choncms");
-		templateVarsMap.put("project-name", "Chon");
-		templateVarsMap.put("project-version", "1.0.0-SNAPSHOT");
+    protected abstract String getProjectType();
 
-		templateVarsMap.put("project-parent-groupId", "com.choncms");
-		templateVarsMap.put("project-parent-artifactId", "bundles");
-		templateVarsMap.put("project-parent-version", "1.0.0-SNAPSHOT");
-		templateVarsMap.put("project-parent-pom-relativePath", "../pom.xml");
-		return templateVarsMap;
-	}
+    protected Map<String, Object> getTemplateVariables() {
+        Map<String, Object> templateVarsMap = new HashMap<String, Object>();
+        templateVarsMap.put("project-groupId", "com.choncms");
+        templateVarsMap.put("project-package", "com.choncms");
+        templateVarsMap.put("project-name", "Chon");
+        templateVarsMap.put("project-version", "0.0.1-SNAPSHOT");
 
-	/**
-	 * @parameter expression="${basedir}" default-value="${user.dir}"
-	 */
-	protected String basedir;
+        templateVarsMap.put("project-parent-groupId", "com.choncms");
+        templateVarsMap.put("project-parent-artifactId", "bundles");
+        templateVarsMap.put("project-parent-version", "0.0.1-SNAPSHOT");
+        templateVarsMap.put("project-parent-pom-relativePath", "../pom.xml");
+        return templateVarsMap;
+    }
 
-	@Override
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		try {
-			Map<String, Object> tplVars = getTemplateVariables();
-			String projectStrictrueXml = getProjectType() + ".structure.xml";
-			Resource project = ProjectStructure.read(projectStrictrueXml,
-					tplVars, resourceLocation);
-			File base = new File(basedir);
-			base.mkdirs();
-			project.create(base);
-		} catch (IOException e) {
-			throw new MojoExecutionException("Error creating project", e);
-		}
-	}
+    /**
+     * @parameter expression="${basedir}" default-value="${user.dir}"
+     */
+    protected String basedir;
 
-	protected String getValue(String description, String defVal)
-			throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		System.out.print("Value for '" + description + "', default (leave blank): '" + defVal
-				+ "': ");
-		String line = br.readLine();
-		if (line == null || line.trim().length() == 0) {
-			return defVal;
-		}
-		return line;
-	}
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        try {
+            Map<String, Object> tplVars = getTemplateVariables();
+            String projectStructrueXml = getProjectType() + ".structure.xml";
+            Resource project = ProjectStructure.read(projectStructrueXml,
+                    tplVars, resourceLocation);
+            File base = new File(basedir);
+            base.mkdirs();
+            project.create(base);
+        } catch (IOException e) {
+            throw new MojoExecutionException("Error creating project: ", e);
+        }
+    }
+
+    protected String getValue(String description, String defVal)
+            throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Value for '" + description
+                + "', default (leave blank): '" + defVal + "': ");
+        String line = br.readLine();
+        if (line == null || line.trim().length() == 0) {
+            return defVal;
+        }
+        return line;
+    }
 
 }
